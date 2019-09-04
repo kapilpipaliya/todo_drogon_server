4d3e44794637cddc5b67351ab3f992d3ed70719c
Kapil <kapil.pipaliya@yahoo.com>
2019-09-04 06:47:58 +0530

    use vcpkg package manger

--- main.cc
+++ main.cc
@@ -1,13 +1,16 @@
 #include <drogon/drogon.h>
 #include "spdlogfix.h"
-
-
-
+#define GSL_THROW_ON_CONTRACT_VIOLATION
+#include <gsl/gsl>
 
 int main()
 {
     spdlog::set_pattern("[source %s] [function %!] [line %#] %v");
-
+    try {
+        Expects(-8 > 0);
+    } catch(const std::exception& e) {
+        SPDLOG_TRACE( "Main Actor Exception Error : {}", e.what());
+    }
     //auto console = spdlog::stdout_logger_mt("console");
     //spdlog::set_default_logger(console);
 
@@ -15,16 +18,13 @@ int main()
     //SPDLOG_LOGGER_TRACE(console, "logger output with arg {}", 2); // [source main.cpp] [function main] [line 17] logger output with arg 2
     //console->info("invoke member function");
 
-
     drogon::app().registerPostHandlingAdvice(
     [](const drogon::HttpRequestPtr &req, const drogon::HttpResponsePtr &resp) {
         //LOG_DEBUG << "postHandling1";
         resp->addHeader("Access-Control-Allow-Origin", "*");
         //resp->addHeader("Access-Control-Allow-Credentials", "true");
     });
-    //Set HTTP listener address and port
     // drogon::app().addListener("0.0.0.0",8300);
-    drogon::app().loadConfigFile("./config.json");
-    drogon::app().run();
+    drogon::app().loadConfigFile("./config.json").run();
     return 0;
 }
