211d4e689a069fdae3abee43e3aca2ded8d56afd
Kapil <kapil.pipaliya@yahoo.com>
2019-09-16 09:32:28 +0530

    removed unused string functions

--- core/strfns.cpp
+++ core/strfns.cpp
@@ -1,21 +1,5 @@
 #include "strfns.h"
 #include <fstream>
-/*
-QString rtrimChar(const QString& s, QChar c)
-{
-    QString r = s.trimmed();
-    while(r.endsWith(c))
-        r.chop(1);
-    return r;
-}
-*/
-std::string rtrimChar(const std::string& s, char c) {
-  auto r = trim_copy(s);
-  if (r.at(r.size() - 1) == c) {
-    r.pop_back();
-  }
-  return s;
-}
 
 // cool function:
 // https://wandbox.org/permlink/ESIFJiztM76Q8KvP
