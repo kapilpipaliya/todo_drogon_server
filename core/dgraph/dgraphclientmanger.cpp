bfb52283311f5609dc07f43e9a2a5bcf61262195
Kapil <kapil.pipaliya@yahoo.com>
2019-10-16 01:48:57 +0530

    add dgraph client manager. make dgraph separate project.

--- core/dgraph/dgraphclientmanger.cpp
+++ core/dgraph/dgraphclientmanger.cpp
@@ -1,24 +1,64 @@
 #include "dgraphclientmanger.h"
 #include <cassert>
-#include "./dgraphclient.h"
-#include "./dgraphclientstub.h"
+#include "./dgraphhttp/dgraphclient.h"
+#include "./dgraphhttp/dgraphclientstub.h"
+#include "./orm/dgraphorm.h"
+
+#include "./dgraphgrpc/dgraphclient.h"
 namespace dgraph {
-std::map<std::string, std::shared_ptr<DgraphClient>>
-    DGraphClientManger::dgraphconns = {};
+
+std::map<std::string, std::shared_ptr<http::DGraphClient>>
+    DGraphClientManger::dgraph_http_clients = {};
+std::map<std::string, std::shared_ptr<grpcclient::DgraphClient>>
+    DGraphClientManger::dgraph_grpc_clients = {};
+std::map<std::string, std::shared_ptr<orm::DGraphOrm>>
+    DGraphClientManger::dgraphorms = {};
 DGraphClientManger::DGraphClientManger() {}
 
-std::shared_ptr<DgraphClient> DGraphClientManger::getDGraphClient(
+std::shared_ptr<http::DGraphClient> DGraphClientManger::getDGraphClient(
     const std::string &name) {
-  assert(dgraphconns.find(name) != dgraphconns.end());
-  return dgraphconns[name];
+  assert(dgraph_http_clients.find(name) != dgraph_http_clients.end());
+  return dgraph_http_clients[name];
 }
 
-std::shared_ptr<DgraphClient> DGraphClientManger::createDGraphClient(
-    const std::string &name, const dgraph::DGraphClientStub &clientstub) {
-  std::vector<DGraphClientStub> clients;
+std::shared_ptr<http::DGraphClient> DGraphClientManger::createDGraphClient(
+    const std::string &name, const dgraph::http::DGraphClientStub &clientstub) {
+  std::vector<http::DGraphClientStub> clients;
   clients.push_back(clientstub);
-  auto ptr = std::make_shared<DgraphClient>(clients);
-  dgraphconns[name] = ptr;
+  auto ptr = std::make_shared<http::DGraphClient>(clients);
+  dgraph_http_clients[name] = ptr;
+  return ptr;
+}
+
+std::shared_ptr<grpcclient::DgraphClient>
+DGraphClientManger::getDGraphGrpcClient(const std::string &name) {
+  assert(dgraph_grpc_clients.find(name) != dgraph_grpc_clients.end());
+  return dgraph_grpc_clients[name];
+}
+
+std::shared_ptr<grpcclient::DgraphClient>
+DGraphClientManger::createDGraphGrpcClient(const std::string &name,
+                                           const std::string &host) {
+  auto stub = api::Dgraph::NewStub(
+      grpc::CreateChannel(host, grpc::InsecureChannelCredentials()));
+  std::vector<std::unique_ptr<api::Dgraph::Stub>> clients;
+  clients.push_back(std::move(stub));
+
+  auto ptr = std::make_shared<dgraph::grpcclient::DgraphClient>(clients);
+  dgraph_grpc_clients[name] = ptr;
+  return ptr;
+}
+
+std::shared_ptr<orm::DGraphOrm> DGraphClientManger::getDGraphOrm(
+    const std::string &name) {
+  assert(dgraphorms.find(name) != dgraphorms.end());
+  return dgraphorms[name];
+}
+
+std::shared_ptr<orm::DGraphOrm> DGraphClientManger::createDGraphOrm(
+    const std::string &name, const std::string &client_name) {
+  auto ptr = std::make_shared<orm::DGraphOrm>(client_name);
+  dgraphorms[name] = ptr;
   return ptr;
 }
 }  // namespace dgraph
--- core/dgraph/dgraphclientmanger.h
+++ core/dgraph/dgraphclientmanger.h
@@ -4,15 +4,41 @@
 #include <memory>
 
 namespace dgraph {
-class DgraphClient;
+namespace orm {
+class DGraphOrm;
+}
+namespace http {
+class DGraphClient;
 class DGraphClientStub;
+}  // namespace http
+namespace grpcclient {
+class DgraphClient;
+}
+
 class DGraphClientManger {
  public:
-  static std::map<std::string, std::shared_ptr<DgraphClient>> dgraphconns;
+  static std::map<std::string, std::shared_ptr<http::DGraphClient>>
+      dgraph_http_clients;
+  static std::map<std::string, std::shared_ptr<grpcclient::DgraphClient>>
+      dgraph_grpc_clients;
+  static std::map<std::string, std::shared_ptr<orm::DGraphOrm>> dgraphorms;
+
   DGraphClientManger();
-  static std::shared_ptr<DgraphClient> getDGraphClient(const std::string &name);
-  static std::shared_ptr<DgraphClient> createDGraphClient(
-      const std::string &name, const dgraph::DGraphClientStub &clientstub);
+  static std::shared_ptr<http::DGraphClient> getDGraphClient(
+      const std::string &name);
+  static std::shared_ptr<http::DGraphClient> createDGraphClient(
+      const std::string &name,
+      const dgraph::http::DGraphClientStub &clientstub);
+
+  static std::shared_ptr<grpcclient::DgraphClient> getDGraphGrpcClient(
+      const std::string &name);
+  static std::shared_ptr<grpcclient::DgraphClient> createDGraphGrpcClient(
+      const std::string &name, const std::string &host);
+
+  static std::shared_ptr<orm::DGraphOrm> getDGraphOrm(const std::string &name);
+  static std::shared_ptr<orm::DGraphOrm> createDGraphOrm(
+      const std::string &name, const std::string &client_name);
 };
+
 }  // namespace dgraph
 #endif  // DGRAPHCLIENTMANGER_H
